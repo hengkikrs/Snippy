7dde0cc528e9291916b4d102d1a28533f04bf1f6
hengkikrs <118151623+hengkikrs@users.noreply.github.com>
2026-04-21 21:11:31 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,26 @@
 import { useRef } from 'react'
 import { Icons } from './Icons'
-import { exportJSON, parseImportFile } from '../utils/storage'
+import { parseImportFile } from '../utils/storage'
 import styles from './Header.module.css'
 
-export function Header({ search, onSearch, darkMode, onToggleDark, onAdd, onImport, snippets }) {
+export function Header({ search, onSearch, darkMode, onToggleDark, onAdd, onImport, onExport }) {
   const fileRef = useRef(null)
-
-  const handleExport = () => exportJSON(snippets)
 
   const handleImport = async (e) => {
     const file = e.target.files[0]
     if (!file) return
-    try {
-      const data = await parseImportFile(file)
-      onImport(data)
-    } catch (err) {
-      alert(err.message)
-    }
+    await onImport(file)
     e.target.value = ''
   }
 
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        {/* Logo */}
         <div className={styles.logo}>
-          <div className={styles.logoIcon}>
-            <Icons.Logo />
-          </div>
-          <span>
-            Snip<em>py</em>
-          </span>
+          <div className={styles.logoIcon}><Icons.Logo /></div>
+          <span>Snip<em>py</em></span>
         </div>
 
-        {/* Search */}
         <div className={styles.searchWrap}>
           <Icons.Search />
           <input
@@ -44,23 +31,14 @@ export function Header({ search, onSearch, darkMode, onToggleDark, onAdd, onImpo
           />
         </div>
 
-        {/* Actions */}
         <div className={styles.actions}>
           <button className="btn hide-mobile" onClick={() => fileRef.current.click()}>
-            <Icons.Import />
-            <span>Import</span>
+            <Icons.Import /><span>Import</span>
           </button>
-          <input
-            ref={fileRef}
-            type="file"
-            accept=".json"
-            style={{ display: 'none' }}
-            onChange={handleImport}
-          />
+          <input ref={fileRef} type="file" accept=".json" style={{ display: 'none' }} onChange={handleImport} />
 
-          <button className="btn hide-mobile" onClick={handleExport}>
-            <Icons.Export />
-            <span>Export</span>
+          <button className="btn hide-mobile" onClick={onExport}>
+            <Icons.Export /><span>Export</span>
           </button>
 
           <button className="btn btn-icon" onClick={onToggleDark} title="Toggle dark mode">
@@ -68,8 +46,7 @@ export function Header({ search, onSearch, darkMode, onToggleDark, onAdd, onImpo
           </button>
 
           <button className="btn btn-primary" onClick={onAdd}>
-            <Icons.Plus />
-            <span>Tambah</span>
+            <Icons.Plus /><span>Tambah</span>
           </button>
         </div>
       </div>
